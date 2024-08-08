d4578e9e4a873f102c35725cdaf94b1538fcebeb
darkhorse-solution <darkhorse.solutionist@gmail.com>
2024-08-08 20:06:36 +0300

    Fix: fix types error for auth middleware

--- middleware/requireAuth.ts
+++ middleware/requireAuth.ts
@@ -3,8 +3,14 @@ import { verifyToken } from '../utils/auth';
 import User from '../models/User';
 import connectToDatabase from '../lib/mongoose';
 
+import { IUser } from '../models/User'; // Assuming your User interface is here
+
+export interface AuthenticatedRequest extends NextApiRequest {
+  user?: IUser;
+}
+
 export default function requireAuth(handler: any) {
-  return async (req: NextApiRequest, res: NextApiResponse) => {
+  return async (req: AuthenticatedRequest, res: NextApiResponse) => {
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
