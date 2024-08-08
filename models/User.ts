3b0d4045f9ab2533bba887dc62f5c0c334910643
darkhorse-solution <darkhorse.solutionist@gmail.com>
2024-08-08 19:40:08 +0300

    Feat: add Video schema

--- models/User.ts
+++ models/User.ts
@@ -1,27 +1,34 @@
-import mongoose, { Document, Model, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-interface IUser extends Document {
-  name: string;
+export interface IUser extends Document {
   email: string;
+  password: string;
   createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
-const UserSchema: Schema<IUser> = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  { timestamps: true }
+);
+
+UserSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
+  next();
 });
 
-const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
+UserSchema.methods.comparePassword = function (candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
+const User = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
 export default User;
